4919dce327159cccf12ff385c9f6d822d701f803
Jackson Wu <jacksonwu226@gmail.com>
2024-01-09 15:32:07 -0800

    Initial commit Added task.js file - created a Task class that handles -- title,description,dueDate, and priority Added UI.js - the purpose is to have a UI class that -- handles the DOM manipulation, not complete yet Added index.js -- This is the main entry point Also using webpack

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Task from './task.js';
+
+const myTask = new Task('Example Task', 'Task description', 2, '2024-01-09');
+console.log(myTask.priority); // Output: 2
---
+++ src/task.js
@@ -0,0 +1,34 @@
+class Task{
+    constructor(title, description, priority, dueDate = 'No due date'){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+    get title() {
+        return this._title;
+    }
+    set title(newTitle){
+        this._title = newTitle;
+    }
+    get description() {
+        return this._description;
+    }
+    set description(newDescription){
+        this._description = newDescription;
+    }
+    get priority(){
+        return this._priority;
+    }
+    set priority(newPriority){
+        this._priority = newPriority;
+    }
+    get dueDate(){
+        return this._dueDate;
+    }
+    set dueDate(newDate){
+        this._dueDate = newDate;
+    }
+}
+
+export default Task;
